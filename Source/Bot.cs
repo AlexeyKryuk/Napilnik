b9b5922615561ddd6a6b8594cad7e01336717e19
AlexeyKryuk <43128289+AlexeyKryuk@users.noreply.github.com>
2021-11-27 23:21:16 +0300

    Source folder

---
+++ Source/Bot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Napilnik.Source
+{
+    public class Bot
+    {
+        public Bot(Weapon weapon)
+        {
+            if (weapon == null)
+                throw new ArgumentNullException();
+
+            Weapon = weapon;
+        }
+
+        public Weapon Weapon { get; private set; }
+
+        public void OnSeePlayer(Player player)
+        {
+            Weapon.Fire(player);
+        }
+    }
+}
---
+++ Source/Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Napilnik.Source
+{
+    public class Player
+    {
+        public Player(int health)
+        {
+            if (health <= 0)
+                throw new ArgumentOutOfRangeException();
+
+            Health = health;
+        }
+
+        public event Action HealthEnded;
+        public event Action<int> HealthChanged;
+
+        public int Health { get; private set; }
+
+        public void ApplyDamage(int damage)
+        {
+            if (damage <= 0)
+                throw new ArgumentOutOfRangeException();
+
+            int expectedHealth = Health - damage;
+
+            if (expectedHealth < 0)
+            {
+                Health = 0;
+                HealthEnded?.Invoke();
+            }
+            else
+            {
+                Health = expectedHealth;
+                HealthChanged?.Invoke(Health);
+            }
+        }
+    }
+}
