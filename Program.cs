305d598cfc5b3c3d13420ec729b298636f098b10
AlexeyKryuk <43128289+AlexeyKryuk@users.noreply.github.com>
2021-12-12 17:48:05 +0300

    Error correction

--- Program.cs
+++ Program.cs
@@ -15,22 +15,20 @@ namespace Napilnik.Source.Task2
             Product iPhone11 = new Product("IPhone 11");
 
             Warehouse warehouse = new Warehouse();
-
             Shop shop = new Shop(warehouse);
-            warehouse.DisplayRemainder();
+
+            ProductViewer productViewer = new ProductViewer();
 
             warehouse.DeliverTo(iPhone12, 10);
             warehouse.DeliverTo(iPhone11, 1);
 
-            warehouse.DisplayRemainder();
-            //Вывод всех товаров на складе с их остатком
+            productViewer.DisplayAll(warehouse.Products); //Вывод всех товаров на складе с их остатком
 
-            Cart cart = shop.GetCart();
+            Cart cart = shop.CreateCart();
             cart.Add(iPhone12, 4);
             cart.Add(iPhone11, 3); //при такой ситуации возникает ошибка так, как нет нужного количества товара на складе
 
-            //Вывод всех товаров в корзине
-            warehouse.DisplayRemainder();
+            productViewer.DisplayAll(cart.Products); //Вывод всех товаров в корзине
 
             Console.WriteLine(cart.CreateOrder().Paylink);
 
---
+++ Warehouse .cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Napilnik.Source.Task2
+{
+    public class Warehouse
+    {
+        private Dictionary<Product, int> _products = new Dictionary<Product, int>();
+
+        public IReadOnlyDictionary<Product, int> Products => _products;
+
+        public void DeliverTo(Product product, int count)
+        {
+            if (product == null)
+                throw new ArgumentNullException(nameof(product));
+            if (count <= 0)
+                throw new ArgumentOutOfRangeException(nameof(count));
+
+            if (_products.ContainsKey(product))
+                _products[product] += count;
+            else
+                _products.Add(product, count);
+        }
+
+        public void DeliverFrom(Product product, int count)
+        {
+            int availability = CheckAvailability(product, count);
+            int countInWarehouse = _products[product];
+            int expectedCount = count;
+
+            switch (availability)
+            {
+                case 1:
+                    _products[product] = countInWarehouse - expectedCount;
+                    if (_products[product] == 0)
+                        _products.Remove(product);
+                    break;
+                case 0:
+                    throw new ArgumentOutOfRangeException(nameof(count));
+                case -1:
+                    throw new ArgumentOutOfRangeException(nameof(product));
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(availability));
+            }
+        }
+
+        private int CheckAvailability(Product product, int count)
+        {
+            if (product == null)
+                throw new ArgumentNullException(nameof(product));
+            if (count <= 0)
+                throw new ArgumentOutOfRangeException(nameof(count));
+
+            if (_products.ContainsKey(product) && _products[product] >= count)
+                return 1;
+            else if (_products.ContainsKey(product) && _products[product] < count)
+                return 0;
+            else
+                return -1;
+        }
+    }
+}
