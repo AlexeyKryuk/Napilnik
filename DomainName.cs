fd1c1f13c1ba22d71ddd6fcd3a960e05276971e0
AlexeyKryuk <43128289+AlexeyKryuk@users.noreply.github.com>
2022-01-22 21:41:46 +0300

    Complete

---
+++ DomainName.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Napilnik
+{
+    public class DomainName
+    {
+        private string _firstLevel;
+        private string _secondLevel;
+        private string _thirdLevel;
+        private string _pathSegment;
+
+        public DomainName(string firstLevel, string secondLevel, string pathSegment, string thirdLevel = "")
+        {
+            if (string.IsNullOrWhiteSpace(secondLevel))
+                throw new ArgumentNullException(secondLevel);
+
+            if (string.IsNullOrWhiteSpace(firstLevel))
+                throw new ArgumentNullException(firstLevel);
+
+            if (string.IsNullOrWhiteSpace(thirdLevel))
+                thirdLevel = "";
+            else
+                thirdLevel = thirdLevel + ".";
+
+            _firstLevel = firstLevel;
+            _secondLevel = secondLevel + ".";
+            _thirdLevel = thirdLevel;
+            _pathSegment = "/" + pathSegment;
+        }
+
+        public string GetName()
+        {
+            return $"{_thirdLevel}{_secondLevel}{_firstLevel}{_pathSegment}?";
+        }
+    }
+}
---
+++ Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Napilnik
+{
+    public class Order
+    {
+        public readonly int Id;
+        public readonly int Amount;
+
+        public Order(int id, int amount)
+        {
+            if (amount < 0)
+                throw new ArgumentOutOfRangeException(nameof(amount));
+
+            Id = id;
+            Amount = amount;
+        }
+    }
+}
