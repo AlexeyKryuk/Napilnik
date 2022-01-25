df081fb77d2df15a564025644eadbd8c45b4b964
AlexeyKryuk <43128289+AlexeyKryuk@users.noreply.github.com>
2022-01-25 17:43:25 +0300

    Complete

--- Program.cs
+++ Program.cs
@@ -8,8 +8,20 @@ namespace Napilnik
 {
     class Program
     {
+        public static int FindIndexOf(int[] array, int element)
+        {
+            for (int i = 0; i < array.Length; i++)
+                if (array[i] == element)
+                    return i;
+
+            return -1;
+        }
+
         static void Main(string[] args)
         {
+            int[] numbers = new int[] { 41, 1, 3, 10, -81 };
+
+            Console.WriteLine(FindIndexOf(numbers, 10));
         }
     }
 }
