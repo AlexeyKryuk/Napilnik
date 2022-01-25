5614a659c47352fc7b09f97b6d028b6faa134f39
AlexeyKryuk <43128289+AlexeyKryuk@users.noreply.github.com>
2022-01-25 19:09:43 +0300

    Complete

--- Program.cs
+++ Program.cs
@@ -6,6 +6,29 @@ using System.Threading.Tasks;
 
 namespace Napilnik
 {
+    class CleanCode_ExampleTask11
+    {
+        private readonly int _armySize;
+        private readonly int _coinsNumber;
+        private readonly string _name;
+
+        public CleanCode_ExampleTask11(int armySize, int coinsNumber, string name)
+        {
+            if (armySize < 0)
+                throw new ArgumentOutOfRangeException(nameof(armySize));
+
+            if (coinsNumber < 0)
+                throw new ArgumentOutOfRangeException(nameof(coinsNumber));
+
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentNullException(name);
+
+            _armySize = armySize;
+            _coinsNumber = coinsNumber;
+            _name = name;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
