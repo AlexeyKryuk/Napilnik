5355e96b7a7f9cfdfa41aa6c08a8ed5e75a4d35e
AlexeyKryuk <43128289+AlexeyKryuk@users.noreply.github.com>
2022-01-25 17:26:31 +0300

    Complete

--- Program.cs
+++ Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace Napilnik
 {
+    public static class Mathf
+    {
+        public static int Median(int a, int b, int c)
+        {
+            if (a < b)
+                return b;
+            else if (a > c)
+                return c;
+            else
+                return a;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            int a = 2, b = 0, c = 1;
+            int median = Mathf.Median(a, b, c);
+
+            Console.WriteLine(median); // 1
         }
     }
 }
