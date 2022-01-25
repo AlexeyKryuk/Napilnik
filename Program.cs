da455002542c55870b7dbe565c47ee0b88004d67
AlexeyKryuk <43128289+AlexeyKryuk@users.noreply.github.com>
2022-01-25 19:57:18 +0300

    Complete

--- Program.cs
+++ Program.cs
@@ -1,11 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Napilnik
 {
+    internal class Player
+    {
+        
+    }
+
+    internal class Weapon
+    {
+        
+    }
+
+    internal class Follower
+    {
+        
+    }
+
+    internal class Unit
+    {
+
+    }
+
+    internal class UnitStorage
+    {
+        public IReadOnlyCollection<Unit> Units { get; private set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
