c3c43259e03076392100d030f3dfdb72e12450a3
AlexeyKryuk <43128289+AlexeyKryuk@users.noreply.github.com>
2022-01-28 22:39:04 +0300

    Naming method

--- Program.cs
+++ Program.cs
@@ -1,32 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Napilnik
+﻿namespace Napilnik
 {
     public static class Mathf
     {
-        public static int Median(int a, int b, int c)
+        public static int Clamp(int value, int min, int max)
         {
-            if (a < b)
-                return b;
-            else if (a > c)
-                return c;
+            if (value < min)
+                return min;
+            else if (value > max)
+                return max;
             else
-                return a;
-        }
-    }
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int a = 2, b = 0, c = 1;
-            int median = Mathf.Median(a, b, c);
-
-            Console.WriteLine(median); // 1
+                return value;
         }
     }
 }
